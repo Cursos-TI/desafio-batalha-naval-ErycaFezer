d0dd98ee1669646891fa6cf86500b2f5ab37ed30
ErycaFezer <erycaaboskyvisky@gmail.com>
2025-03-31 23:13:20 +0000

    VERSAO FINAL BATALHA NAVAL

--- BatalhaNavalOficial.c
+++ BatalhaNavalOficial.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
+#define LINHAS 10
+#define COLUNAS  10
+
 int main(){
+
+    int resultado[LINHAS][COLUNAS];
 
     //Declarando String para localizacao no tabuleiro
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    int i = 0;
-    //Declarando Matriz
-    int Tabuleiro[10][10] = {
+    //Declarando Matrizes
+    int Tabuleiro[LINHAS][COLUNAS] = {
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -16,6 +20,46 @@ int main(){
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    //Matrizes de Efeitos
+    int EfeitoCone[LINHAS][COLUNAS] = {
+        {0, 0, 0, 0, 2, 0, 0, 0, 0, 0},
+        {0, 0, 0, 2, 2, 2, 0, 0, 0, 0},
+        {0, 0, 2, 2, 2, 2, 2, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    int EfeitoCruz[LINHAS][COLUNAS] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 2, 0, 0, 0, 0, 0},
+        {0, 0, 2, 2, 2, 2, 2, 0, 0, 0},
+        {0, 0, 0, 0, 2, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    int EfeitoOctaedro[LINHAS][COLUNAS] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+        {2, 2, 2, 0, 0, 0, 0, 0, 0, 0},
+        {0, 2, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
@@ -41,12 +85,31 @@ int main(){
         }  
     }
     
+    //Sobrepondo as matrizes para a impressao dos efeitos no tabuleiro
     for(int i = 0; i < 10; i ++){
         for(int j = 8; j > 5; j--){
             if(i + j == 8){
                 Tabuleiro[i][j] = 3;
             }
         }  
+    }
+
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            resultado[i][j] = Tabuleiro[i][j] += EfeitoCone[i][j]; 
+        }
+    }
+
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            resultado[i][j] = Tabuleiro[i][j] += EfeitoCruz[i][j]; 
+        }
+    }
+
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            resultado[i][j] = Tabuleiro[i][j] += EfeitoOctaedro[i][j]; 
+        }
     }
 
     //Exibindo O Tabuleiro
@@ -59,17 +122,15 @@ int main(){
 
     printf("\n");
 
-   for(int controle = 0; controle < 10; controle++){
+   for(int i = 0; i < 10; i++){
     printf(" ");
     printf("%d", coluna[i]);
-        i++;
-    for(int i = 0; i < 10; i++){
-        printf(" %d", Tabuleiro[controle][i]);
+    for(int j = 0; j < 10; j++){
+        printf(" %d", Tabuleiro[i][j]);
 }
         printf("\n");
         
 }
-    
  return 0;
  
 }
